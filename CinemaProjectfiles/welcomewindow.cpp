77307b07909e80ba7ad11978bce478a2b3f06f30
Mahinour Abdelgawad <MahinourAbdelgawad@aucegypt.edu>
2024-03-07 22:40:09 +0200

    Final final

--- CinemaProjectfiles/welcomewindow.cpp
+++ CinemaProjectfiles/welcomewindow.cpp
@@ -8,7 +8,7 @@ WelcomeWindow::WelcomeWindow(QString username, int age, QWidget *parent)
 {
     ui->setupUi(this);
     ui->label_hello->setText("Hello, " + username + ", " + QString::number(age));
-    QPixmap pix ("C:\\Users\\mahin\\Desktop\\Uni\\Spring 2024\\CS 2 Lab\\Assignments\\Assignment 1\\CinemaProject - Copy\\welcome.jpg");
+    QPixmap pix ("C:\\Users\\mahin\\Desktop\\Uni\\Spring 2024\\CS 2 Lab\\Assignments\\Assignment 1\\CinemaProject - Copy\\CinemaProjectfiles\\welcome.jpg");
     int w = ui->label_image->width();
     int h = ui->label_image->height();
     ui->label_image->setPixmap(pix.scaled(w, h, Qt::KeepAspectRatio));
